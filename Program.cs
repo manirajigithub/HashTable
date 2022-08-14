c71532a6bb8bc0f49d29392c9fa91b34f389abf1
mani1 <mani1@NANI143>
2022-08-14 18:01:31 +0530

    [Rajeswari]Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace HashTable
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+
+            HashTable hashTable = new HashTable(7);
+            //Hash Table
+            hashTable.Insert("To", 1);
+            Console.WriteLine("To->" + hashTable.GetValue("To"));
+            hashTable.Insert("be", 2);
+            Console.WriteLine("Be->" + hashTable.GetValue("be"));
+            hashTable.Insert("Or", 3);
+            Console.WriteLine("Or->" + hashTable.GetValue("Or"));
+            hashTable.Insert("not", 4);
+            Console.WriteLine("not->" + hashTable.GetValue("not"));
+            hashTable.Insert("to", 5);
+            Console.WriteLine("to->" + hashTable.GetValue("to"));
+            hashTable.Insert("be", 6);
+            Console.WriteLine("be->" + hashTable.GetValue("be"));
+
+            Console.WriteLine("Value of key 'not' is: " + hashTable.GetValue("not"));
+            Console.ReadLine();
+        }
+    }
+}
+
